6549a24b9fc98ff327f324e63861a577456a1ccb
Kriti Kumari <74190629+kritikumari777@users.noreply.github.com>
2022-11-14 11:08:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Hello } from "./components/AFunComponent.js";
 import Message from "./components/BClassComp.js";
 import Profile from './components/CProp';
 import Counter from "./components/DState.js";
 import Resume from './components/EDestructorProp';
-// import FunctionEvent from './components/FunctionEvent';
-// import ClassEvent from './components/GClassEvent';
-// import FunctionCounter from './components/HookFunCounter';
-// import ConditionalComponent from './components/IComditionalComponent';
-// import TernaryOperater from './components/JTernaryOperater';
-// import Product from './components/KProduct';
-//import Form from './components/LForm';
-//import Todo from './components/MTodo';
-//import InlineComponent from './components/OInlineStyling';
+import FunctionEvent from './components/FunctionEvent';
+import ClassEvent from './components/GClassEvent';
+import FunctionCounter from './components/HookFunCounter';
+import ConditionalComponent from './components/IComditionalComponent';
+import TernaryOperater from './components/JTernaryOperater';
+import Product from './components/KProduct';
+import Form from './components/LForm';
+import Todo from './components/MTodo';
+import InlineComponent from './components/OInlineStyling';
 //import StylesheetComponent from './components/PStyleSheet';
-//import ModuleComponent from './components/QModuleStyle';
-//import FragmentComponent from './components/RFragment';
-//import Table from './components/STable';
+import ModuleComponent from './components/QModuleStyle';
+import FragmentComponent from './components/RFragment';
+import Table from './components/STable';
 //import Parent from './components/UParent';
-import Modal from './components/WModal';
+//import Modal from './components/WModal';
 function App() {
   return (
     <div className="App">
       <Hello></Hello>
-     <Message messagecontent=" this is prope"></Message>
-     <Profile name="prop" means=" is Property" ></Profile>
-     <Profile name=" property" means=" is argument"></Profile>
-     <Profile name=" argument" means="that we pass to a function"></Profile>
-     <h6>This is a child of a Profile file</h6>
-     <Counter></Counter>
-     <Resume name="Destructure"></Resume>
-     <Modal></Modal>
-    
+      <Message messagecontent=" this is prope"></Message>
+      <Profile name="prop" means=" is Property" ></Profile>
+      <Profile name=" property" means=" is argument"></Profile>
+      <Profile name=" argument" means="that we pass to a function"></Profile>
+      <h6>This is a child of a Profile file</h6>
+      <Counter></Counter>
+      <Resume name="Destructure"></Resume>
+      <FunctionEvent></FunctionEvent>
+      <ClassEvent></ClassEvent>
+      <FunctionCounter></FunctionCounter>
+      <ConditionalComponent></ConditionalComponent>
+      <TernaryOperater></TernaryOperater>
+      <Product></Product>
+      <Form></Form>
+      <Todo></Todo>
+      <InlineComponent></InlineComponent>
+      <ModuleComponent></ModuleComponent>
+      <FragmentComponent></FragmentComponent>
+      <Table></Table>
 
     </div>
   );
